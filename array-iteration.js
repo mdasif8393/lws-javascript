a3f0887736bf51d3e1835beb21d1ddfe17e9b5ca
unknown <md.asif.8393@gmail.com>
2024-01-04 19:37:12 +0600

    [Done --> array iteration]

---
+++ array-iteration.js
@@ -0,0 +1,85 @@
+////for each
+// const numbers = [45, 4, 9, 16, 25];
+// function myFunction(value, index, array) {
+//     console.log("value", value);
+//     console.log("index", index);
+//     console.log("array", array);
+// }
+
+// numbers.forEach(myFunction);
+
+
+////map
+// const numbers1 = [45, 4, 9, 16, 25];
+// const result = numbers1.map(myFunction);
+
+// function myFunction(value) {
+//     return value * 2;
+// }
+// console.log(result);
+
+
+//// filter
+// const numbers = [45, 4, 9, 16, 25];
+// const newArray = numbers.filter(myFunction);
+
+// function myFunction(value, index, array) {
+//     return value > 10
+// }
+// console.log(newArray);
+
+////reduce
+// const numbers = [45, 4, 9, 16, 25];
+// const reduce = numbers.reduce(myFunction, 10);
+
+// function myFunction(total, value, index, array) {
+//     return total + value;
+// }
+
+// console.log(reduce);
+
+
+
+//// every
+// const numbers = [45, 4, 9, 16, 25];
+// const newArray = numbers.every(myFunction);
+
+// function myFunction(value, index, array) {
+//     return value > 3;
+// }
+
+// console.log(newArray);
+
+////some
+// const numbers = [45, 4, 9, 16, 25];
+// let someOver18 = numbers.some(myFunction);
+
+// function myFunction(value, index, array) {
+//     return value > 18;
+// }
+// console.log(someOver18);
+
+//// form
+// console.log(Array.from("ABCDEFG"));
+
+////iondexof()
+// const fruits = ["Apple", "Banana"];
+// console.log(fruits.indexOf("Apple"));
+
+
+////lastIndexof()
+
+
+////keys
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const keys = fruits.keys();
+// for (let x of keys) {
+//     console.log(x);
+// }
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const f = fruits.entries();
+
+// for (let x of f) {
+//     console.log(x);
+// }
--- array-sort.js
+++ array-sort.js
@@ -1,38 +1,38 @@
 
-////sort
+// //sort
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const sort = fruits.sort();
 // console.log(fruits, sort);
 
-////reverse
+// //reverse
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const reverse = fruits.reverse();
 // console.log(reverse, fruits)
 
 
-////toSorted
+// //toSorted
 // const months = ["Jan", "Feb", "Mar", "Apr"];
 // const sorted = months.toSorted();
 // console.log(months, sorted);
 
 
-////toReserved
+// //toReserved
 // const months = ["Jan", "Feb", "Mar", "Apr"];
 // const reversed = months.toReversed();
 // console.log(months, reversed);
 
-////numeric sort
+// //numeric sort
 // const points = [40, 100, 1, 5, 25, 10];
 // const sortArray = points.sort(function (a, b) { return b - a });
 // console.log(sortArray);
 
-////array random number
+// //array random number
 // const points = [40, 100, 1, 5, 25, 10];
 // const result = points.sort(function () { return 0.5 - Math.random() });
 // console.log(result);
 
 
-////Array min max
+// //Array min max
 // const points = [40, 100, 1, 5, 25, 10];
 // const sortArray = points.sort(function (a, b) {
 //     return a - b
@@ -40,17 +40,17 @@
 // console.log(sortArray[0]);
 // console.log(sortArray[sortArray.length - 1]);
 
-////max array
+// //max array
 // const points = [40, 100, 1, 5, 25, 10];
 // const maxArray = Math.max.apply(null, points);
 // console.log(maxArray);
 
-////min array
+// //min array
 // const points = [40, 100, 1, 5, 25, 10];
 // const minArray = Math.min.apply(null, points);
 // console.log(minArray);
 
-////sort object
+// //sort object
 // const cars = [
 //     { type: "Volvo", year: 2016 },
 //     { type: "Saab", year: 2001 },
