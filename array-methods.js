597750049c5f9550ce57265611ecb9f474483d50
unknown <md.asif.8393@gmail.com>
2024-01-04 16:36:44 +0600

    [Done --> array sort]

--- array-methods.js
+++ array-methods.js
@@ -3,61 +3,61 @@
 // const stringFruits = fruits.toString();
 // console.log(stringFruits);
 
-//join
+// join
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const joinFruits = fruits.join("$# ");
 // console.log(joinFruits);
 
 
-//pop
+// pop
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const newFruit = fruits.push("Lemon");
 // console.log(fruits, newFruit);
 
 
-//shift
+// shift
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const shiftFruit = fruits.shift();
 // console.log(fruits, shiftFruit);
 
 
-//unshift
+// unshift
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const shiftArray = fruits.unshift("Lemon");
 // console.log(fruits, shiftArray);
 
 
-//delete
+// delete
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const deleteArray = delete fruits[0];
 // console.log(fruits, deleteArray);
 
 
-//concat
+// concat
 // const myGirls = ["Cecilie", "Lone"];
 // const myBoys = ["Emil", "Tobias", "Linus"];
 // const arr3 = ["Robin", "Morgan"];
 // const concatArray = myGirls.concat("AAAAA");
 // console.log(concatArray);
 
-//copyWith()
+// copyWith()
 // const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
 // const fruits1 = fruits.copyWithin(2, 0, 2);
 // console.log(fruits, fruits1);
 
 
-//flat
+// flat
 // const myArr = [[1, 2], [3, 4], [5, 6]];
 // const newArr = myArr.flat();
 // console.log(myArr, newArr);
 
 
-//splice
+// splice
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const fruits1 = fruits.splice(2, 2, "Lemon", "Kiwi");
 // console.log(fruits, fruits1);
 
-//slice
-const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-const citrus = fruits.slice(1, 3);
-console.log(citrus);
+// slice
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus);
