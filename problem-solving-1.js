466d86fb2262c39b6c36bb9bceb5f1a7fd887a66
unknown <md.asif.8393@gmail.com>
2024-01-05 16:29:49 +0600

    [Done --> comparison]

--- problem-solving-1.js
+++ problem-solving-1.js
@@ -1,4 +1,4 @@
-//// random number
+// // random number
 
 // function getRandomNumber(min, max) {
 //     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,20 +8,20 @@
 // console.log(randomNumber);
 
 
-//// Name Alphabetically sort
+// // Name Alphabetically sort
 // const students = ["Sumit", "Saad", "Daiyan", "Akask"];
 // console.log(students.sort());
 
 
 
 
-////Number in small to big
+// //Number in small to big
 // const roll_numbers = [3, 5, 6, 2, 7, 8, 4];
 // console.log(roll_numbers.sort(function (a, b) { return a - b }));
 
 
 
-////Find Vowels from a sentance
+// //Find Vowels from a sentance
 // const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
 // function countVowels(sentence) {
@@ -43,5 +43,5 @@
 
 
 
-//// from array extruct duplicate value
+// // from array extruct duplicate value 
 
