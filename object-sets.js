e721355de690e59fdc7e8f70164bfceaf94c0b56
unknown <md.asif.8393@gmail.com>
2024-01-08 16:16:24 +0600

    [Done--> object Map]

---
+++ object-sets.js
@@ -0,0 +1,38 @@
+// const letters = new Set(["a","b","c"]);
+
+
+// const letters = new Set();
+// letters.add("a");
+// letters.add("b");
+// letters.add("c");
+// console.log(letters);
+
+
+
+
+// // Create Variables
+// const a = "a";
+// const b = "b";
+// const c = "c";
+
+// // Create a Set
+// const letters = new Set();
+
+// // Add Variables to the Set
+// letters.add(a);
+// letters.add(b);
+// letters.add(c);
+
+
+
+// const letters = new Set(["add", "a", "add", "b", "add", "c"]);
+// const myIterator = letters.values();
+// for (let x of myIterator) {
+//     console.log(x);
+// }
+
+
+
+// const letters = new Set(["add", "a", "add", "b", "add", "c"]);
+// console.log(letters instanceof Set);
+
