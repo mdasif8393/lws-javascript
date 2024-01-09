4d78691bc103e0500517c11941b0c13e7030bc62
unknown <md.asif.8393@gmail.com>
2024-01-09 12:13:23 +0600

    [Done--> Function Invocation]

---
+++ function-invocation.js
@@ -0,0 +1,24 @@
+// function a() {
+//     console.log(this);
+// }
+// a()
+
+
+// const x = {
+//     a: function a() {
+//         console.log(this);
+//     }
+// }
+// x.a();
+
+
+
+
+// function myFunction(arg1, arg2) {
+//     this.firstName = arg1;
+//     this.lastName = arg2;
+// }
+
+// const sumit = new myFunction("Sumit", "Bhai");
+
+// console.log(sumit.firstName);
