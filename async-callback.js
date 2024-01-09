1c08eafe2077892de7c0c275f737c2f133594cd6
unknown <md.asif.8393@gmail.com>
2024-01-09 21:41:04 +0600

    [Done --> async]

--- async-callback.js
+++ async-callback.js
@@ -27,20 +27,20 @@
 
 
 
-function calculator(num1, num2, callback) {
-    let sum = num1 + num2;
-    if (callback) {
-        callback(sum);
-    }
+// function calculator(num1, num2, callback) {
+//     let sum = num1 + num2;
+//     if (callback) {
+//         callback(sum);
+//     }
 
-    return sum;
+//     return sum;
 
-}
+// }
 
-calculator(5, 5, function (result) {
-    console.log(result);
-});
-console.log(result);
+// calculator(5, 5, function (result) {
+//     console.log(result);
+// });
+// console.log(result);
 
 
 
