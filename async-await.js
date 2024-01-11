05aaf4c77062f1599bd666b827e9df6e3cdbb7bd
unknown <md.asif.8393@gmail.com>
2024-01-11 09:43:52 +0600

    [Done --> Dom methods]

--- async-await.js
+++ async-await.js
@@ -1,66 +1,66 @@
-// const paymentSuccess = true;
-// const marks = 90;
+const paymentSuccess = true;
+const marks = 90;
 
-// function enroll() {
-//     console.log("Course enrollment is in progress");
+function enroll() {
+    console.log("Course enrollment is in progress");
 
-//     const promise = new Promise(function (resolve, reject) {
-//         setTimeout(function () {
-//             if (paymentSuccess) {
-//                 resolve()
-//             }
-//             else {
-//                 reject("Payment failed");
-//             }
-//         }, 2000)
-//     });
+    const promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (paymentSuccess) {
+                resolve()
+            }
+            else {
+                reject("Payment failed");
+            }
+        }, 2000)
+    });
 
-//     return promise;
-// }
+    return promise;
+}
 
-// function progress() {
-//     console.log("Course on progress");
+function progress() {
+    console.log("Course on progress");
 
-//     const promise = new Promise(function (resolve, reject) {
-//         setTimeout(function () {
-//             if (marks > 80) {
-//                 resolve()
-//             }
-//             else {
-//                 reject("You could not get enough marks to get the certificate");
-//             }
-//         }, 2000)
-//     });
+    const promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            if (marks > 80) {
+                resolve()
+            }
+            else {
+                reject("You could not get enough marks to get the certificate");
+            }
+        }, 2000)
+    });
 
-//     return promise;
-// }
+    return promise;
+}
 
 
-// function getCertificate() {
-//     console.log("Preparing your certificate!");
+function getCertificate() {
+    console.log("Preparing your certificate!");
 
-//     const promise = new Promise(function (resolve) {
-//         setTimeout(function () {
-//             resolve("Congratulations! Your certificate is ready");
-//         }, 1000)
-//     })
+    const promise = new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve("Congratulations! Your certificate is ready");
+        }, 1000)
+    })
 
-//     return promise;
-// }
+    return promise;
+}
 
-// async function course() {
-//     try {
-//         await enroll();
-//         await progress();
-//         const message = await getCertificate();
+async function course() {
+    try {
+        await enroll();
+        await progress();
+        const message = await getCertificate();
 
-//         console.log(message);
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// }
+        console.log(message);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
-// course();
+course();
 
 
