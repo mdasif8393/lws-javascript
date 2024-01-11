33460cda9e5b1c1e035f66d8c54517bfd9a73cfb
unknown <md.asif.8393@gmail.com>
2024-01-11 12:21:42 +0600

    [Done --> Dom document]

---
+++ dom-document.js
@@ -0,0 +1,9 @@
+// const x = document.forms["frm1"];
+// console.dir(x)
+// let text = "";
+// for (let i = 0; i < x.length; i++) {
+//     text += x.elements[i].value + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+
