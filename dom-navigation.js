ef352562ad60079951c82f0fa0c4fbd5493cad7f
unknown <md.asif.8393@gmail.com>
2024-01-12 09:11:03 +0600

    [Done --> Dom Navigation]

---
+++ dom-navigation.js
@@ -0,0 +1,16 @@
+// const p = document.getElementById("demo");
+
+// const way1 = p.innerHTML;
+// console.log(way1);
+
+// const way2 = p.firstChild.nodeValue;
+// console.log(way2);
+
+// const way3 = p.childNodes[0].nodeValue;
+// console.log(way3);
+
+// const way4 = p.childNodes[0].nodeName;
+// console.log(way4);
+
+// console.log(document.body);
+// console.log(document.documentElement);
