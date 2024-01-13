232884a6b141d7ca670089ed7ae0a2895dc2b2e9
unknown <md.asif.8393@gmail.com>
2024-01-13 13:11:39 +0600

    [Done --> query selector]

---
+++ js-play-with-dom/script.js
@@ -0,0 +1,39 @@
+// let headerElement = document.getElementById("header");
+// headerElement.style.borderBottom = "3px solid red"
+// headerElement.style.fontSize = "30px";
+
+// console.dir(document)
+
+// console.log(document.getElementsByClassName("item")[0]);
+
+// let itemUl = document.getElementById("items");
+// const items = itemUl.getElementsByClassName("item");
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.color = "red";
+// };
+
+
+// let items = document.getElementsByTagName("img");
+// console.log(items);
+
+
+//Query Selector
+// let header = document.querySelector("#new-task");
+// console.log(header.textContent);
+
+// let newTask = document.querySelector("#new-task");
+// console.log(newTask);
+
+// const lastItem = document.querySelector(".item:last-child");
+// lastItem.style.color = "red";
+
+// const lastItems = document.querySelectorAll(".item:last-child");
+// for (let element of lastItems) {
+//     element.style.color = "red";
+// }
+
+// const lastItem = document.querySelector(".item:nth-child(2)");
+// lastItem.style.color = "red";
+
+// const lastItem = document.querySelector("#items").querySelector(".item:nth-child(2)");
+// lastItem.style.color = "red";
