9c89ab5143b89f883fbbcb247e39577514b1c8ab
unknown <md.asif.8393@gmail.com>
2024-01-13 13:45:06 +0600

    [Done --> parent child]

--- js-play-with-dom/script.js
+++ js-play-with-dom/script.js
@@ -17,7 +17,7 @@
 // console.log(items);
 
 
-//Query Selector
+// Query Selector
 // let header = document.querySelector("#new-task");
 // console.log(header.textContent);
 
@@ -37,3 +37,28 @@
 
 // const lastItem = document.querySelector("#items").querySelector(".item:nth-child(2)");
 // lastItem.style.color = "red";
+
+
+
+
+
+
+//DOM Hierarch
+// const grandParent = document.querySelector(".todo-list")
+// // const parent = grandParent.children;
+// const children = grandParent.querySelectorAll(".item");
+// console.log(children);
+
+
+// const children = document.querySelector(".item");
+// // const parent = children.parentElement;
+// // console.log(parent);
+
+// const grandParent = children.closest(".todo-list");
+// console.log(grandParent);
+
+
+
+// const childrenTwo = document.querySelector('.item').nextElementSibling;
+// const childrenOne = childrenTwo.previousElementSibling;
+// childrenOne.style.color = "red";
