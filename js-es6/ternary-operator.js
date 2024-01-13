a86e87abf316c49f42a26356508c7bcaad0500a0
unknown <md.asif.8393@gmail.com>
2024-01-13 22:02:18 +0600

    [Done--> ternary operator]

---
+++ js-es6/ternary-operator.js
@@ -0,0 +1,13 @@
+var age = 18;
+
+var type = age > 18 ? 'adult'
+    : (age < 10)
+        ? 'child'
+        : 'young';
+
+console.log(type);
+
+
+var a = 5;
+var access = a > 5;
+console.log(access);
