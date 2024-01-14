bfb9f97ae8b900510b87d325a97bed263de6a2a9
unknown <md.asif.8393@gmail.com>
2024-01-14 13:04:40 +0600

    [Done--> array psuh splice]

---
+++ js-es6/array-concat.js
@@ -0,0 +1,6 @@
+var numbers1 = [1, 2, 3, 4, 5, 6, 7, 8];
+var numbers2 = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var numbers3 = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+var result = numbers1.concat(numbers2, numbers3);
+console.log(result);
---
+++ js-es6/array-push.js
@@ -0,0 +1,3 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+numbers.push(11, 32, 44);
+console.log(numbers);
