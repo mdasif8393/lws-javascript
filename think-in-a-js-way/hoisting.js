2e9368cc642847288fae96a7c6c8f9fe6d42a68b
unknown <md.asif.8393@gmail.com>
2024-01-17 14:50:15 +0600

    [Done--> hoisting]

---
+++ think-in-a-js-way/hoisting.js
@@ -0,0 +1,54 @@
+// var a;
+// a = "Bangladesh"
+
+// console.log(a);
+
+
+
+// console.log(a);
+// let a = "Bangladesh";
+
+
+
+
+
+// let LANGUAGE = "Java";
+// let language = "Javascript";
+
+// function getLanguage() {
+//     if (!language) {
+//         let language = LANGUAGE;
+//     }
+//     return language;
+// }
+
+// console.log(`I love ${getLanguage()}`);
+
+
+
+
+// myFunc();
+
+// function myFunc() {
+//     console.log(`I love JavaScript`);
+// }
+
+
+
+
+// function myFunc() {
+//     language = "JavaScript";
+//     var language;
+//     console.log(language);
+// }
+
+// myFunc();
+
+
+
+// myFunc();
+// const myFunc = function () {
+//     language = "JavaScript";
+//     var language;
+//     console.log(language);
+// }
